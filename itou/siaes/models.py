gip-inclusion/les-emplois

Manage SIAE authorization

itou/siaes/models.py
@@ -126,6 +126,16 @@ class Siae(AddressMixin):  # Do not forget the mixin!
     website = models.URLField(verbose_name=_("Site web"), blank=True)
     description = models.TextField(verbose_name=_("Description"), blank=True)
 
+    # Expiration is managed by import_siae.py script only.
+    has_expired = models.BooleanField(
+        verbose_name=_("Déconventionnée"),
+        default=False,
+        help_text=_("Précise si la structure manque de conventionnement valide à ce jour."),
+    )
+    # This expiration date is not enforced and only stored for information
+    # for the admin and/or metabase. Only siae.has_expired is enforced.
+    expires_at = models.DateTimeField(verbose_name=_("Date de fin de conventionnement"), blank=True, null=True)
+
     source = models.CharField(
         verbose_name=_("Source de données"), max_length=20, choices=SOURCE_CHOICES, default=SOURCE_ASP
     )
@@ -133,6 +143,9 @@ class Siae(AddressMixin):  # Do not forget the mixin!
     # the siae objects in ASP's own database. These are supposed to never change,
     # so as long as the ASP keeps including this field in all their exports,
     # it will be easy for us to accurately match data between exports.
+    # Note that this external_id unicity is based on SIRET only.
+    # In other words, if an EI and a ACI share the same SIRET, they also
+    # share the same external_id in ASP's own database.
     external_id = models.IntegerField(verbose_name=_("ID externe"), null=True, blank=True)
 
     jobs = models.ManyToManyField(

itou/www/siaes_views/tests.py
@@ -314,6 +314,7 @@ class CreateSiaeViewTest(TestCase):
         self.assertEqual(new_siae.description, post_data["description"])
         self.assertEqual(new_siae.created_by, user)
         self.assertEqual(new_siae.source, Siae.SOURCE_USER_CREATED)
+        self.assertFalse(new_siae.has_expired)
 
         # This data comes from BAN_GEOCODING_API_RESULT_MOCK.
         self.assertEqual(new_siae.coords, "SRID=4326;POINT (2.316754 48.838411)")
@@ -369,6 +370,7 @@ class CreateSiaeViewTest(TestCase):
         self.assertEqual(new_siae.description, post_data["description"])
         self.assertEqual(new_siae.created_by, user)
         self.assertEqual(new_siae.source, Siae.SOURCE_USER_CREATED)
+        self.assertFalse(new_siae.has_expired)
 
         # This data comes from BAN_GEOCODING_API_RESULT_MOCK.
         self.assertEqual(new_siae.coords, "SRID=4326;POINT (2.316754 48.838411)")

NathHense/itou

itou/siaes/models.py
@@ -126,6 +126,16 @@ class Siae(AddressMixin):  # Do not forget the mixin!
     website = models.URLField(verbose_name=_("Site web"), blank=True)
     description = models.TextField(verbose_name=_("Description"), blank=True)
 
+    # Authorization is managed by import_siae.py script only.
+    is_authorized = models.BooleanField(
+        verbose_name=_("Conventionnée"),
+        default=True,
+        help_text=_("Précise si la structure a un conventionnement valide à ce jour."),
+    )
+    # This authorization end date is not enforced and only stored for
+    # information, for admin and/or metabase uses.
+    authorized_until = models.DateTimeField(verbose_name=_("Date de fin de conventionnement"), blank=True, null=True)
+
     source = models.CharField(
         verbose_name=_("Source de données"), max_length=20, choices=SOURCE_CHOICES, default=SOURCE_ASP
     )
@@ -133,6 +143,9 @@ class Siae(AddressMixin):  # Do not forget the mixin!
     # the siae objects in ASP's own database. These are supposed to never change,
     # so as long as the ASP keeps including this field in all their exports,
     # it will be easy for us to accurately match data between exports.
+    # Note that this external_id unicity is based on SIRET only.
+    # In other words, if an EI and a ACI share the same SIRET, they also
+    # share the same external_id in ASP's own database.
     external_id = models.IntegerField(verbose_name=_("ID externe"), null=True, blank=True)
 
     jobs = models.ManyToManyField(

itou/www/siaes_views/tests.py
@@ -314,6 +314,7 @@ class CreateSiaeViewTest(TestCase):
         self.assertEqual(new_siae.description, post_data["description"])
         self.assertEqual(new_siae.created_by, user)
         self.assertEqual(new_siae.source, Siae.SOURCE_USER_CREATED)
+        self.assertTrue(new_siae.is_authorized)
 
         # This data comes from BAN_GEOCODING_API_RESULT_MOCK.
         self.assertEqual(new_siae.coords, "SRID=4326;POINT (2.316754 48.838411)")
@@ -369,6 +370,7 @@ class CreateSiaeViewTest(TestCase):
         self.assertEqual(new_siae.description, post_data["description"])
         self.assertEqual(new_siae.created_by, user)
         self.assertEqual(new_siae.source, Siae.SOURCE_USER_CREATED)
+        self.assertTrue(new_siae.is_authorized)
 
         # This data comes from BAN_GEOCODING_API_RESULT_MOCK.
         self.assertEqual(new_siae.coords, "SRID=4326;POINT (2.316754 48.838411)")